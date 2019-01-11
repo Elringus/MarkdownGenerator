8209f39581efab43be09b4655dc1e4872e8be273
Artyom Sovetnikov <elringus@yandex.ru>
2019-01-11 20:10:47 +0300

    provide intro text as arg

--- Program.cs
+++ Program.cs
@@ -6,29 +6,15 @@ namespace MarkdownWikiGenerator
 {
     public class Program
     {
-        // 0 = dll src path, 1 = dest root, 2 = namespace regex filter
+        // 0 = dll src path, 1 = dest root, 2 = namespace regex filter, 3 = intro text
         private static void Main(string[] args)
         {
-            //args = new [] { @"C:\Users\Elringus\AppData\Local\Temp\NaninovelDocsGenerator\Assembly-CSharp.dll", @"C:\Users\Elringus\Documents\WebProjects\NaninovelWeb\docs\api", @"^Naninovel" };
+            //args = new [] { @"C:\Users\Elringus\AppData\Local\Temp\NaninovelDocsGenerator\Assembly-CSharp.dll", @"C:\Users\Elringus\Documents\WebProjects\NaninovelWeb\docs\api", @"^Naninovel", "..." };
 
-            var target = string.Empty;
-            var dest = "md";
-            var namespaceMatch = string.Empty;
-            if (args.Length == 1)
-            {
-                target = args[0];
-            }
-            else if (args.Length == 2)
-            {
-                target = args[0];
-                dest = args[1];
-            }
-            else if (args.Length == 3) 
-            {
-                target = args[0];
-                dest = args[1];
-                namespaceMatch = args[2];
-            }
+            var target = args[0];
+            var dest = args[1];
+            var namespaceMatch = args[2];
+            var introText = args[3];
 
             var types = MarkdownGenerator.Load(target, namespaceMatch);
             foreach (var group in types.GroupBy(x => x.Namespace).OrderBy(x => x.Key))
@@ -39,9 +25,7 @@ namespace MarkdownWikiGenerator
                 var fileName = "index.md";
 
                 var sb = new StringBuilder();
-                sb.Append("---\nsidebar: auto\n---\n\n");
-                sb.Append("# API Reference\n\n");
-                sb.Append("Novel script actions API reference. Use the side bar to quickly navigate between available actions. Check out the [novel scripts guide](/guide/novel-scripts.md) in case you have no idea what's this all about.\n\n");
+                sb.Append(introText);
 
                 foreach (var type in group.OrderBy(x => x.HasActionTag ? x.ActionTag : x.Name))
                 {
